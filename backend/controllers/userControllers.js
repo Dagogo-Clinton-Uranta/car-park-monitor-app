72facfe1982039c24ee0584113bef15c456bde73
dagogo <dagogouranta@gmail.com>
2021-06-24 14:19:11 +0100

    fiddling with CORS yet again

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -30,6 +30,7 @@ dotenv.config()
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
   res.header("Access-Control-Allow-Origin","*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   /*const { email } = req.body*/
   //req.body will give us the object thats sent in the body of our front end/POSTMAN JSON, take note
   //res.send accepts an object i think and not just variables, take note...hese are part of the things that you have to research on yor own
@@ -90,7 +91,8 @@ const exitPopulateTicket = asyncHandler(async (req, res) => {
 //@route POST /api/users/parkenter
 //@access Public
 const entryTicketRequest = asyncHandler(async (req, res) => {
-  /*res.header("Access-Control-Allow-Origin","*")*/
+  res.header("Access-Control-Allow-Origin","*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   const { email } = req.body
   //req.body will give us the object thats sent in the body of our front end/POSTMAN JSON, take note
   //res.send accepts an object i think and not just variables, take note...hese are part of the things that you have to research on yor own
